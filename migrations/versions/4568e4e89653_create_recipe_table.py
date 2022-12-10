04e7bfd07dc6eb78e823010fa55adf3edbd1727c
Guillaume TÉCHER <guillaumeval.techer@gmail.com>
2022-12-09 23:03:38 -0500

    better migration

--- migrations/versions/4568e4e89653_create_recipe_table.py
+++ migrations/versions/4568e4e89653_create_recipe_table.py
@@ -1,12 +1,12 @@
 """create recipe table
 
 Revision ID: 4568e4e89653
-Revises: 
+Revises:
 Create Date: 2022-12-02 19:30:30.921232
 
 """
 from alembic import op
-import sqlalchemy as sa 
+import sqlalchemy as sa
 from sqlalchemy.dialects.postgresql import ARRAY
 # revision identifiers, used by Alembic.
 revision = '4568e4e89653'
@@ -18,11 +18,13 @@ depends_on = None
 def upgrade() -> None:
     op.create_table(
         'recipe',
-        sa.Column('id', sa.Integer, primary_key=True),
+        sa.Column('id', sa.Integer, primary_key=True,
+                  autoincrement=True,  nullable=False, unique=True),
         sa.Column('title', sa.String, nullable=False),
         sa.Column('link', sa.String, nullable=False),
         sa.Column('image', sa.String, nullable=True),
-        sa.Column('ingredients', ARRAY(sa.String,dimensions=1), nullable=True),
+        sa.Column('ingredients', ARRAY(
+            sa.String, dimensions=1), nullable=True),
         sa.Column('cook_time', sa.String, nullable=True),
         sa.Column('prep_time', sa.String, nullable=True),
         sa.Column('rest_time', sa.String, nullable=True),
