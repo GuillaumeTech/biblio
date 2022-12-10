f9fcf8d191bfa5ad66d9c98951cbee820966c3ca
Guillaume TÉCHER <guillaumeval.techer@gmail.com>
2022-12-09 23:03:29 -0500

    fix populate script

--- scripts/populate_db.py
+++ scripts/populate_db.py
@@ -1,7 +1,8 @@
 
 import pandas as pd
 from sqlalchemy import create_engine
-import os, sys
+import os
+import sys
 from dotenv import load_dotenv
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -18,18 +19,17 @@ marieclaire = pd.read_json(f'{data_dir}/marieclaire.json', encoding="utf8")
 
 db = create_engine(os.environ["DATABASE_URL"])
 
-ricardo.to_sql('recipe', db, if_exists='replace', index = False)
+ricardo.to_sql('recipe', db, if_exists='append', index=False)
 print("inserted ricardo data")
 
-marmitton.to_sql('recipe', db, if_exists='replace', index = False)
+marmitton.to_sql('recipe', db, if_exists='append', index=False)
 print("inserted marmitton data")
 
-septcinq.to_sql('recipe', db, if_exists='replace', index = False)
+septcinq.to_sql('recipe', db, if_exists='append', index=False)
 print("inserted 750g data")
 
-cuisine_az.to_sql('recipe', db, if_exists='replace', index = False)
+cuisine_az.to_sql('recipe', db, if_exists='append', index=False)
 print("inserted cuisine az data")
 
-marieclaire.to_sql('recipe', db, if_exists='replace', index = False)
+marieclaire.to_sql('recipe', db, if_exists='append', index=False)
 print("inserted marieclaire data")
-
