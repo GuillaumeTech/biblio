f34717ffb9a8a7af5f10ec28c03f59de0332ea50
Guillaume TÉCHER <guillaumeval.techer@gmail.com>
2022-12-09 23:06:18 -0500

    search recipes

--- biblio/routers/recipes.py
+++ biblio/routers/recipes.py
@@ -1,9 +1,23 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+import os
+from biblio.sqla.models import Recipe
+
 
 router = APIRouter(
     prefix="/recipes",
 )
 
-@router.get("/search")
-async def read_items():
-    return fake_items_db
+
+async def get_session():
+    engine = create_engine(os.environ["DATABASE_URL"])
+    Session = sessionmaker(bind=engine)
+
+    return Session()
+
+
+@router.get("/search/{text}")
+async def read_items(text: str, session=Depends(get_session)):
+    recipes = session.query(Recipe).filter(Recipe.title.contains(text))
+    return [recipe for recipe in recipes]
