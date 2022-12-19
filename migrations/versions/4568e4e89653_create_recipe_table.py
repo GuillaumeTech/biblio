44a65d3b80549acfbe62b4d2e30b73994fbb4eb5
Guillaume TÉCHER <guillaumeval.techer@gmail.com>
2022-12-19 15:09:30 -0500

    refactor: better schema

--- migrations/versions/4568e4e89653_create_recipe_table.py
+++ migrations/versions/4568e4e89653_create_recipe_table.py
@@ -7,7 +7,6 @@ Create Date: 2022-12-02 19:30:30.921232
 """
 from alembic import op
 import sqlalchemy as sa
-from sqlalchemy.dialects.postgresql import ARRAY
 # revision identifiers, used by Alembic.
 revision = '4568e4e89653'
 down_revision = None
@@ -23,15 +22,29 @@ def upgrade() -> None:
         sa.Column('title', sa.String, nullable=False),
         sa.Column('link', sa.String, nullable=False),
         sa.Column('image', sa.String, nullable=True),
-        sa.Column('ingredients', ARRAY(
-            sa.String, dimensions=1), nullable=True),
         sa.Column('cook_time', sa.String, nullable=True),
         sa.Column('prep_time', sa.String, nullable=True),
         sa.Column('rest_time', sa.String, nullable=True),
     )
+
+    op.create_table(
+        'ingredient',
+        sa.Column('id', sa.Integer, primary_key=True,
+                  autoincrement=True,  nullable=False, unique=True),
+        sa.Column('recipe_id', sa.Integer, nullable=False),
+        sa.Column('name', sa.String, nullable=True),
+        sa. ForeignKeyConstraint(
+            ["recipe_id"], ["recipe.id"]
+        ),
+    )
+    op.create_index('recipe_title', 'recipe', ['title'])
+    op.create_index('ingredient_name', 'ingredient', ['name'])
+
     pass
 
 
 def downgrade() -> None:
+    op.drop_table('ingredient')
     op.drop_table('recipe')
+
     pass
--- scripts/populate_db.py
+++ scripts/populate_db.py
@@ -4,6 +4,21 @@ from sqlalchemy import create_engine
 import os
 import sys
 from dotenv import load_dotenv
+from tqdm import tqdm
+
+
+def chunker(seq, size):
+    # from http://stackoverflow.com/a/434328
+    return (seq[pos:pos + size] for pos in range(0, len(seq), size))
+
+
+def insert_with_progress(df, db, table_name):
+    chunksize = int(len(df) / 10)  # 10%
+    with tqdm(total=len(df)) as pbar:
+        for i, cdf in enumerate(chunker(df, chunksize)):
+            cdf.to_sql(table_name, db, if_exists='append', index=False)
+            pbar.update(chunksize)
+
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 load_dotenv(os.path.join(BASE_DIR, ".env"))
@@ -17,19 +32,23 @@ septcinq = pd.read_json(f'{data_dir}/750g.json', encoding="utf8")
 cuisine_az = pd.read_json(f'{data_dir}/cuisine_az.json', encoding="utf8")
 marieclaire = pd.read_json(f'{data_dir}/marieclaire.json', encoding="utf8")
 
+frames = [ricardo, marmitton, septcinq, cuisine_az, marieclaire]
+
+result = pd.concat(frames)
+result.insert(0, 'id', range(0, len(result)))
+result = result.explode('ingredients')
+
+ingredients = result.iloc[:, [0, 4]]
+ingredients = ingredients.rename(
+    columns={"id": 'recipe_id', 'ingredients': 'name'})
+
+
+recipes = result.iloc[:, [0, 1, 2, 3, 5, 6, 7]]
+recipes = recipes.drop_duplicates()
+
 db = create_engine(os.environ["DATABASE_URL"])
 
-ricardo.to_sql('recipe', db, if_exists='append', index=False)
-print("inserted ricardo data")
-
-marmitton.to_sql('recipe', db, if_exists='append', index=False)
-print("inserted marmitton data")
-
-septcinq.to_sql('recipe', db, if_exists='append', index=False)
-print("inserted 750g data")
-
-cuisine_az.to_sql('recipe', db, if_exists='append', index=False)
-print("inserted cuisine az data")
-
-marieclaire.to_sql('recipe', db, if_exists='append', index=False)
-print("inserted marieclaire data")
+print("inserting recipes")
+insert_with_progress(recipes, db, 'recipe')
+print("inserting ingredients")
+insert_with_progress(ingredients, db, 'ingredient')
